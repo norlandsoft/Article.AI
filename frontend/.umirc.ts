00a5e77541ba6aab85290a77adfb3d66d82d6052
norlandsoft <11669556@qq.com>
2025-03-13 22:20:01 +0800

    前端框架

---
+++ frontend/.umirc.ts
@@ -0,0 +1,31 @@
+import {defineConfig} from "umi";
+
+export default defineConfig({
+  dva: {},
+  mfsu: false,
+  title: 'Article.AI',
+  links: [
+    {id: 'theme', rel: 'stylesheet', type: 'text/css'},
+    {rel: 'shortcut icon', href: '/favicon.svg'}
+  ],
+  routes: [
+    {
+      path: "/",
+      component: "@/pages/MainFrame"
+    }
+  ],
+  proxy: {
+    // 平台API
+    "/api": {
+      target: "http://localhost:7001",
+      changeOrigin: true,
+      pathRewrite: {"^": ""},
+      'onProxyRes': function (proxyRes, req, res) {
+        proxyRes.headers['Content-Encoding'] = 'chunked';
+      }
+    },
+  },
+  esbuildMinifyIIFE: true,
+  base: "/",
+  outputPath: "dist",
+});
---
+++ frontend/src/pages/MainFrame.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MainFrame:React.FC<any> = props => {
+    return (
+        <div>主要页面</div>
+    );
+}
+
+export default MainFrame;
